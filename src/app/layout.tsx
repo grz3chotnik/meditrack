e0d2464fe3cd1399acb4c179cc75ea7cde117bd5
grz3chotnik <krzysztofkubinski2004@gmail.com>
2026-02-26 20:43:26 -0300

    - vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "@/app/ConvexClientProvider";
+import { Analytics } from "@vercel/analytics/next";
+
 import {
   ClerkProvider,
   SignedIn,
@@ -32,9 +34,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-
-
   return (
     <ClerkProvider>
       <html lang="en">
@@ -44,6 +43,7 @@ export default function RootLayout({
           <ConvexClientProvider>
             <Navbar />
             {children}
+            <Analytics/>
           </ConvexClientProvider>
         </body>
       </html>
